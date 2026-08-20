6d1f9a9740fb8d745675c4ad2f70b65ea770ad68
Admors <admors2309@gmail.com>
2026-08-20 17:09:35 +0200

    ci: ajout pipeline test et déploiement github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: process.env.GITHUB_REPOSITORY
     ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
-    : '/',
+    : './',
 
   build: {
     outDir: 'dist',
