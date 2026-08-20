b72c1aad254a37244551cec765f43d9bdc809c1b
Admors <admors2309@gmail.com>
2026-08-20 17:51:46 +0200

    add: visuel

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: false
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash].js`,
+        chunkFileNames: `assets/[name]-[hash].js`,
+        assetFileNames: `assets/[name]-[hash].[ext]`
+      }
+    }
   },
 
   test: {
